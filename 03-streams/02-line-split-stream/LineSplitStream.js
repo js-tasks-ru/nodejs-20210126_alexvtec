e3acdfd365f0dba085f44be0f3d5f173789c7660
Aleksandr <alx.panteleev@gmail.com>
2021-02-12 08:34:12 +0100

    Solution: 1st part

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,33 @@
 const stream = require('stream');
 const os = require('os');
 
+const newLineCharCode = os.EOL.charCodeAt(0);
+
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    let index = 0;
+
+    for (const symbols of chunk.entries()) {
+      const [idx, sym] = symbols;
+      if (sym === newLineCharCode) {
+        this.push(chunk.slice(index, idx));
+        index += 1;
+      }
+
+      if (idx === chunk.length - 1) {
+        this.push(chunk.slice(idx));
+      }
+    }
+
+    callback(null);
   }
 
   _flush(callback) {
+    callback(null);
   }
 }
 
