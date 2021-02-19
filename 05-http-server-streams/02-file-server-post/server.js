af7e52c6671455b01408f547da009b7491a5f4ab
Aleksandr <alx.panteleev@gmail.com>
2021-02-19 12:38:53 +0100

    Solution: post file

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,12 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+const LimitSizeStream = require('./LimitSizeStream');
+const LimitExceededError = require('./LimitExceededError');
+
+const MAX_FILE_SIZE_BYTES = 1000000;
 
 const server = new http.Server();
 
@@ -11,6 +17,65 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        return res.end(http.STATUS_CODES[400]);
+      }
+
+      const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitStream = new LimitSizeStream({limit: MAX_FILE_SIZE_BYTES});
+
+      req
+          .on('error', (e) => {
+            // Handle connection reset
+            if (e.code === 'ECONNRESET') {
+              fs.unlink(filepath, (err) => {
+                if (err) {
+                  // Log error into system, that file was not deleted
+                  console.log(err);
+                }
+              });
+            }
+          })
+          .pipe(limitStream)
+          .on('error', (e) => {
+            // Handle exceeding the limit
+            if (e instanceof LimitExceededError) {
+              fs.unlink(filepath, (err) => {
+                if (err) {
+                  // Log error into system, that file was not deleted
+                  console.log(err);
+                }
+              });
+              res.statusCode = 413;
+              return res.end(http.STATUS_CODES[413]);
+            }
+          })
+          .pipe(writeStream)
+          .on('finish', () => {
+            // Handle successfully created file
+            res.statusCode = 201;
+            return res.end(http.STATUS_CODES[201]);
+          })
+          .on('abort', () => {
+            // Handle connection abort
+            fs.unlink(filepath, (err) => {
+              if (err) {
+                // Log error into system, that file was not deleted
+                console.log(err);
+              }
+            });
+          })
+          .on('error', (e) => {
+            // Handle existing file
+            if (e.code === 'EEXIST') {
+              res.statusCode = 409;
+              return res.end(http.STATUS_CODES[409]);
+            }
+            // Handle other exceptions
+            res.statusCode = 500;
+            return res.end(http.STATUS_CODES[500]);
+          });
 
       break;
 
