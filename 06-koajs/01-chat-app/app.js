9112b4402d18b41274ab41b1e7fcc43dc6c312e1
Aleksandr <alx.panteleev@gmail.com>
2021-02-21 08:52:15 +0100

    Solution: chat on koa

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,43 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
-});
+const subscribers = {};
+
+function subscribe(on, handler) {
+  if (subscribers[on]) {
+    subscribers[on] = [...subscribers[on], handler];
+    return;
+  }
+  subscribers[on] = [handler];
+}
+
+function emit(event, msg) {
+  if (subscribers[event]) {
+    subscribers[event].forEach((h) => h(msg));
+    delete subscribers[event];
+  }
+}
+
+router.get(
+    '/subscribe',
+    async (ctx, next) => {
+      return new Promise((res) => {
+        subscribe(
+            'message',
+            function(msg) {
+              ctx.body = msg;
+              return next(res());
+            });
+      });
+    });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  if (message) {
+    emit('message', message);
+  }
+  ctx.body = message;
+  return next();
 });
 
 app.use(router.routes());
